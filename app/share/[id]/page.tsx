d66d67af320c85035a911f69304ce1bd65e28c2c
Izabelle <izabelle.sdn@gmail.com>
2023-11-20 20:52:13 -0300

    test size 2

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -4,7 +4,7 @@ import { notFound } from 'next/navigation'
 import { formatDate } from '@/lib/utils'
 import { getSharedChat } from '@/app/actions'
 import { ChatList } from '@/components/chat-list'
-import { FooterText } from '@/components/footer'
+// import { FooterText } from '@/components/footer'
 
 export const runtime = 'edge'
 export const preferredRegion = 'home'
@@ -47,7 +47,7 @@ export default async function SharePage({ params }: SharePageProps) {
         </div>
         <ChatList messages={chat.messages} />
       </div>
-      <FooterText className="py-8" />
+      {/* <FooterText className="py-8" /> */}
     </>
   )
 }
